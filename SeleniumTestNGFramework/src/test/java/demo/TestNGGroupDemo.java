2b2fe02e1b0a8d8b261dd65bd9b3e7a6e55806e9
mwickramanayaka <manulwick@gmail.com>
2021-06-15 22:46:36 +0530

    Meta Groups,Regular Expressions, Define in class level

--- SeleniumTestNGFramework/src/test/java/demo/TestNGGroupDemo.java
+++ SeleniumTestNGFramework/src/test/java/demo/TestNGGroupDemo.java
@@ -2,30 +2,44 @@ package demo;
 
 import org.testng.annotations.Test;
 
+@Test(groups= {"AllClassTests"})
 public class TestNGGroupDemo {
 	
-	@Test
+	@Test(groups = {"sanity"})
 	public void test1() {
-		System.out.println("This is test1");
+		System.out.println("This is Test 1 ");
 		
 	}
 	
-	@Test
+	@Test(groups = {"sanity","smoke"})
 	public void test2() {
-		System.out.println("This is test1");
+		System.out.println("This is Test 2 ");
 		
 	}
 	
-	@Test
+	@Test(groups = {"sanity","regression"})
 	public void test3() {
-		System.out.println("This is test1");
+		System.out.println("This is Test 3 ");
 		
 	}
 	
 	@Test
 	public void test4() {
-		System.out.println("This is test1");
+		System.out.println("This is Test 4 ");
 		
 	}
+	
+	@Test(groups = {"windows.regression"})
+	public void test5() {
+		System.out.println("This is Test 5 ");
+		
+	}
+	
+	@Test(groups = {"linux.regression"})
+	public void test6() {
+		System.out.println("This is Test 6 ");
+		
+	}
+	
 
 }
