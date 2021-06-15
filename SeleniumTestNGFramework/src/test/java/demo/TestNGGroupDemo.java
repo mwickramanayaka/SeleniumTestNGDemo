cea1054ee4b583d7c887ffc1c4998d17cc48d52b
mwickramanayaka <manulwick@gmail.com>
2021-06-15 22:06:02 +0530

    Test cases

--- SeleniumTestNGFramework/src/test/java/demo/TestNGGroupDemo.java
+++ SeleniumTestNGFramework/src/test/java/demo/TestNGGroupDemo.java
@@ -1,5 +1,31 @@
 package demo;
 
+import org.testng.annotations.Test;
+
 public class TestNGGroupDemo {
+	
+	@Test
+	public void test1() {
+		System.out.println("This is test1");
+		
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("This is test1");
+		
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("This is test1");
+		
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("This is test1");
+		
+	}
 
 }
