9741099e4484fc8bfcc5df23f572e8913bedc594
mwickramanayaka <manulwick@gmail.com>
2021-06-16 00:05:34 +0530

    Regular expression on Group Dependencies

--- SeleniumTestNGFramework/src/test/java/demo/TestNGDependency.java
+++ SeleniumTestNGFramework/src/test/java/demo/TestNGDependency.java
@@ -4,19 +4,19 @@ import org.testng.annotations.Test;
 
 public class TestNGDependency {
 	
-	@Test(dependsOnGroups = {"sanity1"}) //(dependsOnMethods= {"test2", "test3"}, priority = 0)
+	@Test(dependsOnGroups = {"sanity.*"}) //(dependsOnMethods= {"test2", "test3"}, priority = 0)
 	public void test1() {
 		System.out.println("I am inside test 1 ");
 		
 	}
 
-	@Test (groups= {"sanity1"})
+	@Test (groups= {"sanity 1"})
 	public void test2() {
 		System.out.println("I am inside test 2 ");
 		
 	}
 	
-	@Test 
+	@Test  (groups= {"sanity 2"})
 	public void test3() {
 		System.out.println("I am inside test 3 ");
 		
